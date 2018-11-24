61b877874cf65be2b277d83ab62bd3f98f8ae2c1
Unknown <oceane.tribudubois@gmail.com>
2018-11-24 14:10:06 +0100

    All Activity are created

--- UTooSeeApp/app/src/main/java/fr/volvo/utc/utoosee/Summary.java
+++ UTooSeeApp/app/src/main/java/fr/volvo/utc/utoosee/Summary.java
@@ -1,33 +1,21 @@
 package fr.volvo.utc.utoosee;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
-public class NSerie extends AppCompatActivity {
+public class Summary extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nserie);
+        setContentView(R.layout.activity_summary);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-        Button submit = (Button) findViewById(R.id.submit);
-        submit.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick (View view){
-                Intent intent = new Intent (NSerie.this, presentationComponent.class);
-                startActivity(intent);
-
-            }
-        });
 
 
     }
