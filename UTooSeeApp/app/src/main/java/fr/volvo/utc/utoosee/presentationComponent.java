0c27b64a958358f1958339fe0756122903afef5e
Unknown <oceane.tribudubois@gmail.com>
2018-11-24 19:38:30 +0100

    API Camera in progress

--- UTooSeeApp/app/src/main/java/fr/volvo/utc/utoosee/presentationComponent.java
+++ UTooSeeApp/app/src/main/java/fr/volvo/utc/utoosee/presentationComponent.java
@@ -2,14 +2,21 @@ package fr.volvo.utc.utoosee;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+
 
 public class presentationComponent extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +44,13 @@ public class presentationComponent extends AppCompatActivity {
 
             }
         });
+
+
+
+
+
+
+
 
         Button tripleGrouserShoe = (Button) findViewById(R.id.tripleGrouserShoe);
        /* tripleGrouserShoe.setOnClickListener(new View.OnClickListener(){
