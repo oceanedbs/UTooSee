3637b7897b99507540e6d40bb25530ca4b8fdb56
Unknown <oceane.tribudubois@gmail.com>
2018-11-24 13:16:40 +0100

    Layout : 3 activities done

--- UTooSeeApp/app/src/main/java/fr/volvo/utc/utoosee/NSerie.java
+++ UTooSeeApp/app/src/main/java/fr/volvo/utc/utoosee/NSerie.java
@@ -1,11 +1,13 @@
 package fr.volvo.utc.utoosee;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class NSerie extends AppCompatActivity {
 
@@ -16,6 +18,16 @@ public class NSerie extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        Button submit = (Button) findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View view){
+                Intent intent = new Intent (NSerie.this, presentationComponent.class);
+                startActivity(intent);
+
+            }
+        });
 
 
             //public void onClick(View view) {
